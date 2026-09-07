dcba4551bceec353115474e3825dec86a1c7b62e
Shawn Vibert <humanmaximuscodex@gmail.com>
2026-09-07 14:36:25 -0700

    Stop injecting the broken legacy intelligence distribution layer

--- CLOUDFLARE-INTELLIGENT-HUB-V7/worker.js
+++ CLOUDFLARE-INTELLIGENT-HUB-V7/worker.js
@@ -3,9 +3,7 @@ export default {
     const url = new URL(request.url);
     if (url.pathname === '/' || url.pathname === '/index.html' || url.pathname.startsWith('/intelligence/')) {
       const response = await env.ASSETS.fetch(new Request(new URL('/index.html', request.url), request));
-      const html = await response.text();
-      const upgraded = html.replace('</body>', '<script src="/v7-intelligence-distribution.js"></script></body>');
-      return new Response(upgraded, { status: response.status, headers: new Headers(response.headers) });
+      return new Response(response.body, { status: response.status, headers: new Headers(response.headers) });
     }
     return env.ASSETS.fetch(request);
   }
